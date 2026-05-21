7a78f36cc411d295b6cda1e69bfe44429dd2748c
工口漫画家 <16875185+gongkou-cartoonist@user.noreply.gitee.com>
2026-05-21 12:50:52 +0000

    删除文件 lostitem

--- lostitem/grzx/backend/routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from .auth import auth_bp
-from .profile import profile_bp
-from .password import password_bp
-from .phone import phone_bp
--- lostitem/grzx/backend/utils/__init__.py
+++
@@ -1,9 +0,0 @@
-from .validators import (
-    generate_verification_code,
-    create_verification,
-    verify_code,
-    create_token,
-    decode_token,
-    token_required,
-    allowed_file
-)
