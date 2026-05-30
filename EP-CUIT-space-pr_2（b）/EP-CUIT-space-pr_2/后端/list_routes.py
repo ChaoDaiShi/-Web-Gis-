38ecbfb5ffc38cd56a4600eeca2b2274538b2fcd
工口漫画家 <16875185+gongkou-cartoonist@user.noreply.gitee.com>
2026-05-30 13:48:24 +0000

    删除文件 EP-CUIT-space-pr_2（b）

--- EP-CUIT-space-pr_2（b）/EP-CUIT-space-pr_2/后端/list_routes.py
+++
@@ -1,7 +0,0 @@
-from Start import app
-
-print("All registered routes:")
-print("-" * 60)
-for rule in app.url_map.iter_rules():
-    methods = ','.join(sorted(rule.methods))
-    print(f"{rule.rule:50s} {methods:20s} {rule.endpoint}")
